c9cd14b5b94be47c3f0e85d3ae722255423ba130
Nathan Franck <75099696+NathanFranck@users.noreply.github.com>
2021-04-22 13:26:33 -0600

    Error stuff

--- index.ts
+++ index.ts
@@ -3,24 +3,25 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 (async () => {
-    const div_hi = document.createElement("div");
-    div_hi.innerHTML = `Hello World`;
-    document.body.appendChild(div_hi);
-    navigator.serviceWorker.register('https://tacticaldan.github.io/sw.js', { scope: 'https://tacticaldan.github.io/firebase-cloud-messaging-push-scope' });
 
-    initializeApp({
-        apiKey: "AIzaSyBhbxJ2LCX0iD298QVVsNHGbfJ7v3N-ajk",
-        authDomain: "chill-point.firebaseapp.com",
-        projectId: "chill-point",
-        storageBucket: "chill-point.appspot.com",
-        messagingSenderId: "399143735956",
-        appId: "1:399143735956:web:10eb8083ae993ccf99da81"
-    });
-    getAnalytics();
-
-    // Get registration token. Initially this makes a network call, once retrieved
-    // subsequent calls to getToken will return from cache.
     try {
+        const div_hi = document.createElement("div");
+        div_hi.innerHTML = `Hello World`;
+        document.body.appendChild(div_hi);
+        navigator.serviceWorker.register('https://tacticaldan.github.io/sw.js', { scope: 'https://tacticaldan.github.io/firebase-cloud-messaging-push-scope' });
+
+        initializeApp({
+            apiKey: "AIzaSyBhbxJ2LCX0iD298QVVsNHGbfJ7v3N-ajk",
+            authDomain: "chill-point.firebaseapp.com",
+            projectId: "chill-point",
+            storageBucket: "chill-point.appspot.com",
+            messagingSenderId: "399143735956",
+            appId: "1:399143735956:web:10eb8083ae993ccf99da81"
+        });
+        getAnalytics();
+
+        // Get registration token. Initially this makes a network call, once retrieved
+        // subsequent calls to getToken will return from cache.
         const messaging = getMessaging();
         const registration = await navigator.serviceWorker.register('./sw.js');
         const token = await getToken(messaging, {
