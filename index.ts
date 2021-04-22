b61b8861a33506c6bae70fd9e9bb37e2f9c26d9a
Nathan Franck <75099696+NathanFranck@users.noreply.github.com>
2021-04-22 13:09:21 -0600

    Try try again

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
     const div_hi = document.createElement("div");
     div_hi.innerHTML = `Hello World`;
     document.body.appendChild(div_hi);
-    navigator.serviceWorker.register('/Notifier/sw.js', { scope: '/Notifier/' });
+    navigator.serviceWorker.register('Notifier/sw.js', { scope: '/' });
 
     initializeApp({
         apiKey: "AIzaSyBhbxJ2LCX0iD298QVVsNHGbfJ7v3N-ajk",
